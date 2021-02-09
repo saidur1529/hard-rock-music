f6c22f200229308c6e3bf0fb0075eb2a2f436283
saidur1529 <saidur.rahman04@northsouth.edu>
2021-02-09 21:27:48 +0600

    hard rock music done

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+// const searchSongs = async () => {
+//     const searchText = document.getElementById('search-field').value;
+//     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+//     // load data
+
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displaySongs(data.data);
+// }
+
 const searchSongs = () => {
     const searchText = document.getElementById('search-field').value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
     // load data
+
     fetch(url)
-        .then(res => res.json())
-        .then(data => displaySongs(data.data));
+    .then (res => res.json())
+    .then (data => displaySongs(data.data))
+    .catch(error => displayError('Something went Wrong!! Please try again later!'));
+    
 }
 
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
+    songContainer.innerHTML = '';
     songs.forEach(song => {
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3';
@@ -21,8 +35,38 @@ const displaySongs = songs => {
                </audio>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
             </div>`;
         songContainer.appendChild(songDiv);
     });
+}
+
+// const getLyric = (artist, title) => {
+//     const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+//     fetch(url)
+//         .then(res = res => res.json())
+//         .then(data => displayLyrics(data.lyrics))
+// }
+
+const getLyric = async (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLyrics(data.lyrics);
+    }
+    catch (error) {
+        displayError('Sorry I failed to load lyrics!! Please try again later!');
+    }
+
+}
+
+const displayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics');
+    lyricsDiv.innerText = lyrics;
+}
+
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText = error ;
 }
